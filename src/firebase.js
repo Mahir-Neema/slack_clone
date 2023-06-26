9c01f093ade01551140f2aced1eb71945c5fc11c
Mahir-Neema <mohi10503@gmail.com>
2023-06-26 19:43:16 +0530

    base setup done, react router dom, material ui installed

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAarOxpqblHFgnppUK_ioTg99BEZtx56QY",
+  authDomain: "slack-clone-mahir.firebaseapp.com",
+  projectId: "slack-clone-mahir",
+  storageBucket: "slack-clone-mahir.appspot.com",
+  messagingSenderId: "280431073261",
+  appId: "1:280431073261:web:3d912d8c24a4216a44f9e4",
+  measurementId: "G-K0S88MXJKT"
+};
